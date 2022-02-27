c56d535f4eb4d66e2af0290c1c32789529075322
Yuki Miyagi <61782737+yukimyg@users.noreply.github.com>
2022-02-27 17:19:12 +0900

    feat: add private subnet

--- terraform-aws-basics/subnets.tf
+++ terraform-aws-basics/subnets.tf
@@ -11,3 +11,14 @@ resource "aws_subnet" "public" {
   ]
 }
 
+resource "aws_subnet" "private" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.main.id
+  tags = {
+    Name = "private-subnet"
+  }
+
+  depends_on = [
+    aws_vpc.main
+  ]
+}
