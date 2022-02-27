ec72714613b99997723063b01e749ba9a05f8812
Yuki Miyagi <61782737+yukimyg@users.noreply.github.com>
2022-02-27 17:12:39 +0900

    feat: define EC2 in public subnet

---
+++ terraform-aws-basics/ec2.tf
@@ -0,0 +1,45 @@
+data "aws_ami" "amzn2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-*-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amzn2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  tags = {
+    Name = "WebServer"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
+
+  depends_on = [
+    data.aws_ami.amzn2
+  ]
+}
+
