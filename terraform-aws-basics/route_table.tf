2a5b95b5782c7f4ab6292996b57b5483afddd1b0
Yuki Miyagi <61782737+yukimyg@users.noreply.github.com>
2022-02-27 17:21:58 +0900

    feat: add route table for private subnet

--- terraform-aws-basics/route_table.tf
+++ terraform-aws-basics/route_table.tf
@@ -27,3 +27,31 @@ resource "aws_route_table_association" "public" {
   ]
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  depends_on = [
+    aws_vpc.main
+  ]
+}
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.natgw.id
+  destination_cidr_block = "0.0.0.0/0"
+
+  depends_on = [
+    aws_nat_gateway.natgw,
+    aws_route_table.private
+  ]
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+
+  depends_on = [
+    aws_subnet.private,
+    aws_route_table.private
+  ]
+}
