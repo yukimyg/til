f3561e0c7537a1e5a7ab4ff087001baf886d0040
Yuki Miyagi <61782737+yukimyg@users.noreply.github.com>
2022-03-03 12:42:45 +0900

    fix: add aws_eip and fix nat gateway

--- terraform-aws-basics/gateways.tf
+++ terraform-aws-basics/gateways.tf
@@ -2,15 +2,24 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   depends_on = [
-    aws_vpc.main
+    aws_vpc.main,
   ]
 }
 
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "natgw"
+  }
+}
+
 resource "aws_nat_gateway" "natgw" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.private.id
+  subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat.id
 
   depends_on = [
-    aws_subnet.private
+    aws_subnet.private,
+    aws_eip.nat,
   ]
 }
